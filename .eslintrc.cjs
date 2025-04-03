a790cb2da98b6f5b080de073f625f76d0f2cdf86
G.C.M. <charithmanujaya1@gmail.com>
2025-04-04 04:19:00 +0530

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,15 +6,29 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended', // Add TypeScript support
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parser: '@typescript-eslint/parser', // Use TypeScript parser
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true, // Enable JSX syntax
+    },
+  },
+  settings: {
+    react: {
+      version: '18.2',
+    },
+  },
+  plugins: ['react-refresh', '@typescript-eslint'], // Add TypeScript plugin
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': 'warn', // Treat this as a warning
+    'no-unused-vars': 'warn', // Warn instead of error
+    '@typescript-eslint/no-unused-vars': 'warn', // TypeScript-specific rule for unused vars
+    'react/prop-types': 'off', // If you're using TypeScript, you can disable PropTypes rule
+    'react/react-in-jsx-scope': 'off', // Not required with React 17 JSX Transform
+    // Add other warnings or disable rules here as needed
   },
 }
