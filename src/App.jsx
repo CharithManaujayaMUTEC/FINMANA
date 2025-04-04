de85c6e7a177a921b6efbc51c7ced3f0b2e26e09
Aysha <ayshaajward@gmail.com>
2025-04-04 11:27:58 +0530

    Bb

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 import Income from "./Pages/Income";
 import Expenditure from "./Pages/Expenditure";
-import Bankbalance from "./Pages/Unused/Bankbalance";
 import Footer from "./Pages/Footer";
 import Bankbalance from "./Pages/Bankbalance";
 
